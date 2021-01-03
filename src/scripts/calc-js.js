9d9f1e7d8e4b3cba68032ef8bce9ff3ef19660fc
Rodrigo Junior <rodrigojunior0323@gmail.com>
2021-01-03 09:18:42 -0300

    update

--- src/scripts/calc-js.js
+++ src/scripts/calc-js.js
@@ -17,7 +17,7 @@ function verificar() {
       gênero = "Homem";
       if (idade >= 0 && idade < 3) {
         // Recém Nascido - Masculino
-        imagem.setAttribute("src", "/src/images/crianca-masculina.png");
+        imagem.setAttribute("src", "/projects-javascript/src/images/crianca-masculina.png");
       } else if (idade >= 4 && idade < 8) {
         // Criança - Masculino
       } else if (idade >= 9 && idade < 12) {
@@ -41,7 +41,7 @@ function verificar() {
       gênero = "Feminino";
       if (idade >= 0 && idade < 3) {
         // Recém Nascido - Masculino
-        imagem.setAttribute("src", "/src/images/bebe-feminina.jpg");
+        imagem.setAttribute("src", "/projects-javascript/src/images/bebe-feminina.jpg");
       } else if (idade >= 4 && idade < 8) {
         // Criança - Masculino
       } else if (idade >= 9 && idade < 12) {
